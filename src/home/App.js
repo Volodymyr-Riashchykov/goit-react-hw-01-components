d5bab2a0bddd6341af4c0dd223cc87f414d4ae7e
Wladimyr-R <wlwar@ua.fm>
2021-10-29 19:53:47 +0300

    grouping

--- src/home/App.js
+++ src/home/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import User from './components/user/User';
-import user from './data/user.json'
-import Statistic from './components/statistics/Statistic';
-import statisticalData from './data/statistical-data.json';
-import FriendList from './components/friend/Friend'
-import friends from './data/friends.json'
-import Transaction from './components/transaction/Transaction';
-import transactions from './data/transactions.json';
+import User from '../components/user/User';
+import user from '../data/user.json'
+import Statistic from '../components/statistics/Statistic';
+import statisticalData from '../data/statistical-data.json';
+import FriendList from '../components/friend/Friend'
+import friends from '../data/friends.json'
+import Transaction from '../components/transaction/Transaction';
+import transactions from '../data/transactions.json';
 function App() {
   return (
     <>
